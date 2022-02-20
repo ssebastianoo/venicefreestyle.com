c14722caad59e04437231809805158a6f75bab24
ssebastianoo <sebastianogirotto05@gmail.com>
2022-02-20 13:03:40 +0100

    icons

--- main.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.get('/', (req, res) => {
-    res.redirect('https://www.instagram.com/venice_freestyle/');
-});
-
-app.listen(2030);
