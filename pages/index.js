7143bd1b8b6ca862d763e5fb7f2f5f6c9f4518e7
ssebastianoo <sebastianogirotto05@gmail.com>
2022-06-02 11:03:18 +0200

    head

--- pages/index.js
+++ pages/index.js
@@ -1,44 +1,48 @@
 import Header from "../components/Header";
 import Grid from "../components/Grid";
+import Head from "next/head";
 
 export default function Home() {
+    const items = [
+        {
+            title: "1 Lorem ipsum",
+            content:
+                "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae, ut adipisci. Soluta accusantium, adipisci dignissimos natus eum dolores illo animi?",
+        },
+        {
+            title: "2 Lorem ipsum",
+            content:
+                "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae, ut adipisci. Soluta accusantium, adipisci dignissimos natus eum dolores illo animi?",
+        },
+        {
+            title: "3 Lorem ipsum",
+            content:
+                "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae, ut adipisci. Soluta accusantium, adipisci dignissimos natus eum dolores illo animi?",
+        },
+        {
+            title: "4 Lorem ipsum",
+            content:
+                "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae, ut adipisci. Soluta accusantium, adipisci dignissimos natus eum dolores illo animi?",
+        },
+        {
+            title: "5 Lorem ipsum",
+            content:
+                "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae, ut adipisci. Soluta accusantium, adipisci dignissimos natus eum dolores illo animi?",
+        },
+        {
+            title: "6 Lorem ipsum",
+            content:
+                "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae, ut adipisci. Soluta accusantium, adipisci dignissimos natus eum dolores illo animi?",
+        },
+    ];
+    
     return (
         <div className="home">
+            <Head>
+                <title>VFL</title>
+            </Head>
             <Header />
-            <Grid
-                items={[
-                    {
-                        title: "1 Lorem ipsum",
-                        content:
-                            "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae, ut adipisci. Soluta accusantium, adipisci dignissimos natus eum dolores illo animi?",
-                    },
-                    {
-                        title: "2 Lorem ipsum",
-                        content:
-                            "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae, ut adipisci. Soluta accusantium, adipisci dignissimos natus eum dolores illo animi?",
-                    },
-                    {
-                        title: "3 Lorem ipsum",
-                        content:
-                            "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae, ut adipisci. Soluta accusantium, adipisci dignissimos natus eum dolores illo animi?",
-                    },
-                    {
-                        title: "4 Lorem ipsum",
-                        content:
-                            "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae, ut adipisci. Soluta accusantium, adipisci dignissimos natus eum dolores illo animi?",
-                    },
-                    {
-                        title: "5 Lorem ipsum",
-                        content:
-                            "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae, ut adipisci. Soluta accusantium, adipisci dignissimos natus eum dolores illo animi?",
-                    },
-                    {
-                        title: "6 Lorem ipsum",
-                        content:
-                            "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae, ut adipisci. Soluta accusantium, adipisci dignissimos natus eum dolores illo animi?",
-                    },
-                ]}
-            />
+            <Grid items={items} />
         </div>
     );
 }
