82a8b7535b7403c0c76c7acd28287c598a1dd5c8
ssebastianoo <sebastianogirotto05@gmail.com>
2022-06-02 16:21:28 +0200

    customize grid content

--- components/Grid.jsx
+++ components/Grid.jsx
@@ -1,5 +1,4 @@
 export default function Grid({ items }) {
-
     const gridMaxItems = 6;
 
     let gridItems = [];
@@ -17,29 +16,56 @@ export default function Grid({ items }) {
         }
     });
 
-    if (tempGrid.length > 0 && tempGrid.length < gridMaxItems || tempGrid.length == gridMaxItems) gridItems.push(tempGrid);
-
+    if (
+        (tempGrid.length > 0 && tempGrid.length < gridMaxItems) ||
+        tempGrid.length == gridMaxItems
+    ) {
+        gridItems.push(tempGrid);
+    }
 
     return (
         <div className="grids">
             {gridItems.map((grid, index_) => {
                 return (
                     <div className="grid" key={index_}>
-                    {grid.map((item, index) => {
-                        return (
-                            <div
-                                className={
-                                    "grid-item" + " grid-item-" + (index + 1)
-                                }
-                                key={index}
-                            >
-                                <h3>{item.title}</h3>
-                                <p>{item.content}</p>
-                            </div>
-                        );
-                    })}
-                </div>
-                )
+                        {grid.map((item, index) => {
+                            return (
+                                <div
+                                    className={
+                                        "grid-item" +
+                                        " grid-item-" +
+                                        (index + 1)
+                                    }
+                                    key={index}
+                                    style={{
+                                        backgroundImage: `linear-gradient(
+                                            to bottom,
+                                            rgba(0, 0, 0, 0.7) 0%,
+                                            rgba(0, 0, 0, 0.7) 100%
+                                        ), url("${item.background}")`,
+                                    }}
+                                >
+                                    <h3>{item.title}</h3>
+                                    <p>{item.content}</p>
+                                    <div className="project-links">
+                                        {item.links
+                                            ? item.links.map((link, index) => {
+                                                  return (
+                                                      <a
+                                                          href={link.url}
+                                                          className="project-link"
+                                                      >
+                                                          {link.label}
+                                                      </a>
+                                                  );
+                                              })
+                                            : null}
+                                    </div>
+                                </div>
+                            );
+                        })}
+                    </div>
+                );
             })}
         </div>
     );
--- pages/index.js
+++ pages/index.js
@@ -5,38 +5,71 @@ import Head from "next/head";
 export default function Home() {
     const items = [
         {
-            title: "Uraban Bootcamp by Tau Marin",
-            content: 'Partner: Tau Marin\n\nTag: #bootcamp #Socialnetwork #videocampaign #creativecontest #telegramchat\n\nLink_01: https://www.instagram.com/p/CRBnMCpLaW4\nLink_02: https://www.tau-marin.it/magazine/a-tu-per-tu-con-i-ragazzi-di-vfl/',
+            title: "Urban Bootcamp by Tau Marin",
+            content:
+                "Partner: Tau Marin\n\nTag: #bootcamp #Socialnetwork #videocampaign #creativecontest #telegramchat",
+            background:
+                "https://indexmedical.it/wp-content/uploads/2015/05/LogoTauMarin-704x340.jpg",
+            links: [
+                {
+                    label: "Open",
+                    url: "https://www.tau-marin.it/magazine/a-tu-per-tu-con-i-ragazzi-di-vfl/",
+                },
+                {
+                    label: "Instagram",
+                    url: "https://www.instagram.com/p/CRBnMCpLaW4\nLink_02",
+                },
+            ],
         },
         {
-            title: "2 Lorem ipsum",
-            content:
-                "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae, ut adipisci. Soluta accusantium, adipisci dignissimos natus eum dolores illo animi?",
+            title: "From Venice to…",
+            content: "Tag: #Travel #Video #Report #Streaming",
+            background:
+                "https://www.amadriapark.com/wp-content/uploads/sites/5/2018/08/ap-excursion-Venice.jpg",
+            links: [
+                {
+                    label: "YouTube",
+                    url: "https://youtu.be/UME82USgOMY",
+                },
+            ],
         },
         {
-            title: "3 Lorem ipsum",
-            content:
-                "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae, ut adipisci. Soluta accusantium, adipisci dignissimos natus eum dolores illo animi?",
+            title: " #unsaltoinavanti | ON AIR",
+            content: "Tag: #live #Streaming #Format #Events",
+            background:
+                "https://sportbusinessmag.sport-press.it/wp-content/uploads/sites/5/2020/08/COVER_1080x1080-1024x1024.png",
+            links: [
+                {
+                    label: "YouTube",
+                    url: "https://youtu.be/7SDhrXgjvdQ",
+                },
+            ],
         },
         {
-            title: "4 Lorem ipsum",
+            title: "STOP IT! Covid 19",
+            background:
+                "https://sportbusinessmag.sport-press.it/wp-content/uploads/sites/5/2020/08/COVER_1080x1080-1024x1024.png",
             content:
-                "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae, ut adipisci. Soluta accusantium, adipisci dignissimos natus eum dolores illo animi?",
+                "Partner: Croce Rossa Italiana\n\nTag: #unsaltoinavanti #Crocerossaitaliana #crowfounding",
+            links: [
+                {
+                    label: "YouTube",
+                    url: "https://youtu.be/yl6J7spQnHk",
+                },
+            ],
         },
         {
-            title: "5 Lorem ipsum",
+            title: "SKATE THE MUSEUM",
             content:
-                "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae, ut adipisci. Soluta accusantium, adipisci dignissimos natus eum dolores illo animi?",
-        },
-        {
-            title: "6 Lorem ipsum",
-            content:
-                "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae, ut adipisci. Soluta accusantium, adipisci dignissimos natus eum dolores illo animi?",
-        },
-        {
-            title: "6 Lorem ipsum",
-            content:
-                "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae, ut adipisci. Soluta accusantium, adipisci dignissimos natus eum dolores illo animi?",
+                "Partner: Punta della dogana / Palazzo Grassi _ Fondazione Penault\n\nTag: #skateboarding #venezia #muesum #artnight",
+            background:
+                "https://www.4actionsport.it/wp-content/uploads/2018/06/skate-the-museum.jpg",
+            links: [
+                {
+                    url: "https://www.palazzograssi.it/it/eventi/tutti/art-night-skate-the-museum/",
+                    label: "Open",
+                },
+            ],
         },
         {
             title: "6 Lorem ipsum",
