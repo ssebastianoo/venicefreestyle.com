e518cc750d1030d0347d4135bdae9992e9706725
ssebastianoo <sebastianogirotto05@gmail.com>
2022-06-04 02:00:55 +0200

    aaaaaaaa

--- pages/index.jsx
+++ pages/index.jsx
@@ -8,15 +8,16 @@ export default function Home() {
     const [projects, setProjects] = useState([]);
 
     console.log('a')
-
-    async function getProjects() {
-        const querySnapshot = await getDocs(collection(db, "projects"));
-        const projects = querySnapshot.docs.map((doc) => doc.data());
-        setProjects(projects);
-        console.log('b')
-        console.log(projects)
-    }
-    useEffect(() => getProjects, []);
+    useEffect(() => {
+        async function getProjects() {
+            const querySnapshot = await getDocs(collection(db, "projects"));
+            const projects = querySnapshot.docs.map((doc) => doc.data());
+            setProjects(projects);
+            console.log('b')
+            console.log(projects)
+        }
+        getProjects();
+    }, []);
 
     return (
         <div className="home">
