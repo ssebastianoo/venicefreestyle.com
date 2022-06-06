5930e6167341b1a0c05a5924ec6d2c7f38cdfc47
ssebastianoo <sebastianogirotto05@gmail.com>
2022-06-07 00:31:51 +0200

    mobile

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,33 +1,104 @@
-import Image from "next/image";
-
 export default function Nav() {
     return (
         <div className="nav">
-            <div className="left">
+            <div className="section desktop left">
                 <p>Follow us</p>
                 <a href="https://www.facebook.com/venicefreestylelab/">
-                    <Image src="https://firebasestorage.googleapis.com/v0/b/venicefreestylelab-4289d.appspot.com/o/buttons%2Ffacebook.png?alt=media&token=70ceef56-48de-4a8d-93fe-45dfe7b8641a" width="40" height="40" />
+                    <img
+                        src="https://firebasestorage.googleapis.com/v0/b/venicefreestylelab-4289d.appspot.com/o/buttons%2Ffacebook.png?alt=media&token=5e5957e7-a8c6-4bec-a28a-84a4e7c24d93"
+                        alt="Social"
+                    />
                 </a>
                 <a href="https://www.instagram.com/venice_freestyle/">
-                    <Image src="https://firebasestorage.googleapis.com/v0/b/venicefreestylelab-4289d.appspot.com/o/buttons%2Finstagram.png?alt=media&token=7d3ae8b1-a5bf-4dae-8123-9317976a7f99" width="40" height="40" />
+                    <img
+                        src="https://firebasestorage.googleapis.com/v0/b/venicefreestylelab-4289d.appspot.com/o/buttons%2Finstagram.png?alt=media&token=f427896e-898e-4667-b0ac-70cc3a2721f9"
+                        alt="Social"
+                    />
                 </a>
                 <a href="https://www.youtube.com/channel/UCY8VRN7zA_otXCbU3UoQueg">
-                    <Image src="https://firebasestorage.googleapis.com/v0/b/venicefreestylelab-4289d.appspot.com/o/buttons%2Fyoutube.png?alt=media&token=04d36499-22f5-4de0-a735-6e493b966a9a" width="40" height="40" />
+                    <img
+                        src="https://firebasestorage.googleapis.com/v0/b/venicefreestylelab-4289d.appspot.com/o/buttons%2Fyoutube.png?alt=media&token=a356d6c7-54f9-444a-871f-5ae081da697e"
+                        alt="Social"
+                    />
                 </a>
                 <a href="https://www.linkedin.com/company/freestyle-in-venice-srl/">
-                    <Image src="https://firebasestorage.googleapis.com/v0/b/venicefreestylelab-4289d.appspot.com/o/buttons%2Flinkedin.png?alt=media&token=84da56b8-6ac1-464d-b31b-1a2fd3a16b85" width="40" height="40" />
+                    <img
+                        src="https://firebasestorage.googleapis.com/v0/b/venicefreestylelab-4289d.appspot.com/o/buttons%2Flinkedin.png?alt=media&token=02820750-ab1c-45c5-8ae5-8f43b1baac1d"
+                        alt="Social"
+                    />
                 </a>
             </div>
-            <div className="right">
+            <div className="section desktop right">
                 <p>Contact us</p>
                 <a href="https://t.me/vflcrew">
-                    <Image src="https://firebasestorage.googleapis.com/v0/b/venicefreestylelab-4289d.appspot.com/o/buttons%2Ftelegram.png?alt=media&token=b66d59a5-f17b-4607-a235-fc5bf790b0df" width="40" height="40" />
+                    <img
+                        src="https://firebasestorage.googleapis.com/v0/b/venicefreestylelab-4289d.appspot.com/o/buttons%2Ftelegram.png?alt=media&token=a3abb81a-a9c6-4916-9ed5-02a4ce9468fc"
+                        alt="Social"
+                    />
                 </a>
                 <a href="#">
-                    <Image src="https://firebasestorage.googleapis.com/v0/b/venicefreestylelab-4289d.appspot.com/o/buttons%2Fmessenger.png?alt=media&token=a05689ca-4c0a-403a-bae6-0e72ff232ad3" width="40" height="40" />
+                    <img
+                        src="https://firebasestorage.googleapis.com/v0/b/venicefreestylelab-4289d.appspot.com/o/buttons%2Fmessenger.png?alt=media&token=372e388d-36ea-4a05-b886-921fef30708a"
+                        alt="Social"
+                    />
                 </a>
                 <a href="mailto:info@venicefreestyle.com">
-                    <Image src="https://firebasestorage.googleapis.com/v0/b/venicefreestylelab-4289d.appspot.com/o/buttons%2Fmail.png?alt=media&token=f764e178-ff8b-49fd-a6f2-2faf3739a344" width="40" height="40" />
+                    <img
+                        src="https://firebasestorage.googleapis.com/v0/b/venicefreestylelab-4289d.appspot.com/o/buttons%2Fmail.png?alt=media&token=866db05f-0dad-4127-8bab-388e5a361844"
+                        alt="Social"
+                    />
+                </a>
+            </div>
+            <div className="section mobile">
+                <div className="logo">
+                    <img
+                        src="https://firebasestorage.googleapis.com/v0/b/venicefreestylelab-4289d.appspot.com/o/logos%2Flogo-white.png?alt=media&token=30d9e716-1656-4352-a5bf-a8e85e16f73d"
+                        alt="VFL"
+                    />
+                </div>
+                <a href="https://t.me/vflcrew">
+                    <img
+                        src="https://firebasestorage.googleapis.com/v0/b/venicefreestylelab-4289d.appspot.com/o/buttons%2Ftelegram_minimal.png?alt=media&token=dc5aced1-4701-44da-9b31-be81ac513289"
+                        alt="Social"
+                    />
+                </a>
+                <a href="#">
+                    <img
+                        src="https://firebasestorage.googleapis.com/v0/b/venicefreestylelab-4289d.appspot.com/o/buttons%2Fmessenger_minimal.png?alt=media&token=c5f32ce4-4635-4300-a67a-8e537d1321ee"
+                        alt="Social"
+                    />
+                </a>
+                <a href="mailto:info@venicefreestyle.com">
+                    <img
+                        src="https://firebasestorage.googleapis.com/v0/b/venicefreestylelab-4289d.appspot.com/o/buttons%2Fmail_minimal.png?alt=media&token=11e0b546-01e5-49a0-bb39-83a48c42ad37"
+                        alt="Social"
+                    />
+                </a>
+            </div>
+            <div className="section mobile">
+                <a href="https://www.facebook.com/venicefreestylelab/">
+                    <img
+                        src="https://firebasestorage.googleapis.com/v0/b/venicefreestylelab-4289d.appspot.com/o/buttons%2Ffacebook_minimal.png?alt=media&token=18ed5add-9233-454c-a3d9-a95857421e43"
+                        alt="Social"
+                    />
+                </a>
+                <a href="https://www.instagram.com/venice_freestyle/">
+                    <img
+                        src="https://firebasestorage.googleapis.com/v0/b/venicefreestylelab-4289d.appspot.com/o/buttons%2Finstagram_minimal.png?alt=media&token=35187922-a63e-4919-9fdc-6c34c165e2f0"
+                        alt="Social"
+                    />
+                </a>
+                <a href="https://www.youtube.com/channel/UCY8VRN7zA_otXCbU3UoQueg">
+                    <img
+                        src="https://firebasestorage.googleapis.com/v0/b/venicefreestylelab-4289d.appspot.com/o/buttons%2Fyoutube_minimal.png?alt=media&token=b4959f1f-cc52-4167-ae26-a650fc6d68d0"
+                        alt="Social"
+                    />
+                </a>
+                <a href="https://www.linkedin.com/company/freestyle-in-venice-srl/">
+                    <img
+                        src="https://firebasestorage.googleapis.com/v0/b/venicefreestylelab-4289d.appspot.com/o/buttons%2Flinkedin_minimal.png?alt=media&token=16a3f7d7-27ac-4959-bb25-40b126034744"
+                        alt="Social"
+                    />
                 </a>
             </div>
         </div>
