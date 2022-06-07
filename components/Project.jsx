ebca946c689acc5983768c678f3450bc96cecc4f
ssebastianoo <sebastianogirotto05@gmail.com>
2022-06-07 10:28:22 +0200

    loading spinner

--- components/Project.jsx
+++ components/Project.jsx
@@ -1,57 +1,54 @@
 import SocialButton from "./SocialButton";
 
+import { useState } from "react";
+
 export default function Project({ project, index }) {
+    const [imgLoaded, setImgLoaded] = useState(false);
+
     return (
-        <div
-            className={
-                "grid-item" +
-                " grid-item-" +
-                (index + 1)
-            }
-            key={index}
-            style={{
-                backgroundImage: `url("${project.background}"`,
-                transitionDuration: "0.5s",
-            }}
-        >
-            <div
-                className={
-                    "grid-item-content " +
-                    ([3, 5, 7, 10, 11].includes(
-                        index + 1
-                    )
-                        ? "horizontal"
-                        : "vertical")
-                }
-            >
-                <h3>
-                    {project.title}
-                </h3>
-                <p>{project.content}</p>
-                <div className="project-links">
-                    {project.links
-                        ? project.links.map(
-                              (link, _index) => {
-                                  return (
-                                      <a
-                                          href={
-                                              link.url
-                                          }
-                                          className="project-link"
-                                          key={_index}
-                                      >
-                                          <SocialButton
-                                              url={
-                                                  link.url
-                                              }
-                                          />
-                                      </a>
-                                  );
-                              }
-                          )
-                        : null}
+        <>
+            {imgLoaded ? (
+                <div
+                    className={"grid-item" + " grid-item-" + (index + 1)}
+                    key={index}
+                    style={{
+                        backgroundImage: `url("${project.background}"`,
+                        transitionDuration: "0.5s",
+                    }}
+                >
+                    <div
+                        className={
+                            "grid-item-content " +
+                            ([3, 5, 7, 10, 11].includes(index + 1)
+                                ? "horizontal"
+                                : "vertical")
+                        }
+                    >
+                        <h3>{project.title}</h3>
+                        <p>{project.content}</p>
+                        <div className="project-links">
+                            {project.links
+                                ? project.links.map((link, _index) => {
+                                      return (
+                                          <a
+                                              href={link.url}
+                                              className="project-link"
+                                              key={_index}
+                                          >
+                                              <SocialButton url={link.url} />
+                                          </a>
+                                      );
+                                  })
+                                : null}
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
+            ) : (
+                <div className="loading">
+                    <div class="lds-spinner"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+                    <img style={{display: 'none'}} src={project.background} onLoad={() => setImgLoaded(true)} />
+                </div>
+            )}
+        </>
     );
 }
